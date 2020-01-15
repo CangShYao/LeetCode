3cafeccf9f0a73ca7bea6c24b41375b63706ee0b
CangShYao <1937636639@qq.com>
2020-01-15 22:04:57 +0800

    unfinished 021

---
+++ others/021/021.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+ };
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode *handle = l1->val > l2->val ? l1 : l2;
+        ListNode *ans = handle;
+        while(handle != NULL && handle != NULL)
+        {
+            
+        }
+        return l1;
+    }
+};
