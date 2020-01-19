b6b36483fd4be05ee621944e915eb4e296330031
CangShYao <1937636639@qq.com>
2020-01-19 16:22:28 +0800

    交换值
    Signed-off-by: CangShYao <1937636639@qq.com>

--- others/024/024.cpp
+++ others/024/024.cpp
@@ -6,40 +6,79 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+// swap value
+
+
+#include <algorithm>
 #include <iostream>
+#include <sstream>
+#include <stack>
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <sstream>
 using namespace std;
 
-struct ListNode {
+struct ListNode
+{
     int val;
-    ListNode * next;
+    ListNode *next;
     ListNode(int x) : val(x), next(NULL) {}
 };
 
-class Solution {
+class Solution
+{
+private:
+protected:
 public:
-    ListNode* swapPairs(ListNode* head) {
+    ListNode *swapPairs(ListNode *head)
+    {
         // TODO 完成你的函数
-        return head;
+        ListNode *handle = head;
+        ListNode *ans_head = head;
+        ListNode *ans = ans_head;
+        int temp = -1;
+        int m_size = 2;
+        int count = 1;
+        while (handle != NULL)
+        {
+            if (count % m_size == 0)
+            {
+                ans_head->val = handle->val;
+                handle->val = temp;
+                temp = -1;
+                count++;
+                handle = handle->next;
+                ans_head = handle;
+            }
+            else
+            {
+                temp = handle->val;
+                handle = handle->next;
+                count++;
+            }
+        }
+        return ans;
     }
 };
 
-void trimLeftTrailingSpaces(string &input) {
+#pragma region Input funcation
+void trimLeftTrailingSpaces(string &input)
+{
     input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
-        return !isspace(ch);
-    }));
+                    return !isspace(ch);
+                }));
 }
 
-void trimRightTrailingSpaces(string &input) {
+void trimRightTrailingSpaces(string &input)
+{
     input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
-        return !isspace(ch);
-    }).base(), input.end());
+                    return !isspace(ch);
+                }).base(),
+                input.end());
 }
 
-vector<int> stringToIntegerVector(string input) {
+vector<int> stringToIntegerVector(string input)
+{
     vector<int> output;
     trimLeftTrailingSpaces(input);
     trimRightTrailingSpaces(input);
@@ -48,20 +87,23 @@ vector<int> stringToIntegerVector(string input) {
     ss.str(input);
     string item;
     char delim = ',';
-    while (getline(ss, item, delim)) {
+    while (getline(ss, item, delim))
+    {
         output.push_back(stoi(item));
     }
     return output;
 }
 
-ListNode* stringToListNode(string input) {
+ListNode *stringToListNode(string input)
+{
     // Generate list from the input
     vector<int> list = stringToIntegerVector(input);
 
     // Now convert that list into linked list
-    ListNode* dummyRoot = new ListNode(0);
-    ListNode* ptr = dummyRoot;
-    for(int item : list) {
+    ListNode *dummyRoot = new ListNode(0);
+    ListNode *ptr = dummyRoot;
+    for (int item : list)
+    {
         ptr->next = new ListNode(item);
         ptr = ptr->next;
     }
@@ -70,25 +112,31 @@ ListNode* stringToListNode(string input) {
     return ptr;
 }
 
-string listNodeToString(ListNode* node) {
-    if (node == nullptr) {
+string listNodeToString(ListNode *node)
+{
+    if (node == nullptr)
+    {
         return "[]";
     }
 
     string result;
-    while (node) {
+    while (node)
+    {
         result += to_string(node->val) + ", ";
         node = node->next;
     }
     return "[" + result.substr(0, result.length() - 2) + "]";
 }
+#pragma endregion
 
-int main() {
+int main()
+{
     string line;
-    while (getline(cin, line)) {
-        ListNode* head = stringToListNode(line);
-        
-        ListNode* ret = Solution().swapPairs(head);
+    while (getline(cin, line))
+    {
+        ListNode *head = stringToListNode(line);
+
+        ListNode *ret = Solution().swapPairs(head);
 
         string out = listNodeToString(ret);
         cout << out << endl;
